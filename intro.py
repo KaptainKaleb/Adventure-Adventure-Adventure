7b37cd4f4b880fa4388e80a24ebb55b9bd8a1a88
KaptainKaleb <kmill2019@gmail.com>
2017-07-22 14:29:44 -0500

    Reorganized code, fixed class issue, changed dialouge
    Took out the statement that restated your class and name. To many Global errors that I dont know how to fix! Changed the dialouge to compesate for the deleted Statement. Reorganized code, though it may look a lot worse now... IDK.

--- intro.py
+++ intro.py
@@ -5,35 +5,31 @@ def main():
 		time.sleep(3)
 		print "Just a little while longer!"
 		time.sleep(2)
-	def create_man():
-			print "Welcome To ADVENTUE ADVENTURE ADVENTURE!!"
-			time.sleep(2)
-			user_name = raw_input("Please State Your Name: ")
-			print "Hello there " + user_name + "! Nice to meet you!"
-			time.sleep(2)
-			print "There are four classes..."
-			time.sleep(1)
-			print "The Knight class, The Wizard class, The Monk class, and the Theif class!"
-			class_list = ['Knight', 'Wizard', 'Monk', 'Theif']
+	fake_load()
+	print "Welcome To ADVENTUE ADVENTURE ADVENTURE!!"
+	time.sleep(2)
+	user_name = raw_input("Please State Your Name: ")
+	print "Hello there " + user_name + "! Nice to meet you!"
+	time.sleep(2)
+	print "There are four classes..."
+	time.sleep(1)
+	print "The Knight class, The Wizard class, The Monk class, and the Theif class!"
+	time.sleep(3)
+	def check_class():
+		user_class = raw_input("Please choose your class: ")
+		class_list = ['Knight', 'Wizard', 'Monk', 'Theif']
+		if user_class in class_list:
 			time.sleep(3)
+			print "Wait..... This cant be"
+			time.sleep(3)
+			print "Is this really you???"
+			time.sleep(3)
+			print "You.... You are him?"
+			time.sleep(3)
+			print "Come with me. Quickly!"
+		else:
+			print "You've Entered Something Wrong! Please Try Again!"
 			user_class = raw_input("Please choose your class: ")
-			for cal in class_list:
-				if user_class != cal:
-					print "You entered somehting wrong! Make sure you type in your class exactly how it is displayed!"
-				else:
-					time.sleep(3)
-					print "Wait..... This cant be"
-					time.sleep(3)
-					print user_name + " of the " + user_class + "s" + "????"
-					time.sleep(3)
-					print "Is this really you???"
-					time.sleep(3)
-					break
-				break
-	#fake_load()
-	create_man()
+			check_class()
+	check_class()
 main()
-
-
-
-		
