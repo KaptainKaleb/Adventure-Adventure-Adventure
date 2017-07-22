7a2d5f03bae31eed3c6b3e2b6ec6add1a2d112bc
KaptainKaleb <kmill2019@gmail.com>
2017-07-21 22:36:30 -0500

    Attempting to fix Class bug
    Trying to use an if/else statement to fix my class problem

--- intro.py
+++ intro.py
@@ -17,19 +17,16 @@ def main():
 			class_list = ["Knight", "Wizard", "Monk", "Theif"]
 			time.sleep(3)
 			user_class = raw_input("Please choose your class: ")
-			while True:
-				if user_class != class_list:
-					print "You entered somehting wrong! Make sure you type in your class exactly how it is displayed!"
-					time.sleep(3)
-			return
-			
-			time.sleep(3)
-			print "Wait..... This cant be"
-			time.sleep(3)
-			print user_name + " of the " + user_class + "s" + "????"
-			time.sleep(3)
-			print "Is this really you???"
-			time.sleep(3)
+			if user_class != class_list:
+				print "You entered somehting wrong! Make sure you type in your class exactly how it is displayed!"
+			else:
+				time.sleep(3)
+				print "Wait..... This cant be"
+				time.sleep(3)
+				print user_name + " of the " + user_class + "s" + "????"
+				time.sleep(3)
+				print "Is this really you???"
+				time.sleep(3)
 	fake_load()
 	create_man()
 main()
