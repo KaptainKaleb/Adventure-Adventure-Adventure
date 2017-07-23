10ba2416ba13d52da5893e803fd4638b9bb37e47
KaptainKaleb <kmill2019@gmail.com>
2017-07-22 21:22:55 -0500

    Changed the Theme Back and forth :(

--- Main/intro.py
+++ Main/intro.py
@@ -6,23 +6,23 @@ def main():
 		print "Just a little while longer!"
 		time.sleep(2)
 	fake_load()
-	print "Welcome To ADVENTUE ADVENTURE ADVENTURE!!"
+	print "Welcome To Some Cool Adventure Name!!"
 	time.sleep(2)
 	user_name = raw_input("Please State Your Name: ")
 	print "Hello there " + user_name + "! Nice to meet you!"
 	time.sleep(2)
-	print "There are four classes..."
+	print "There are four classs..."
 	time.sleep(1)
-	print "The Knight class, The Wizard class, The Monk class, and the Thief class!"
+	print "The Class1 Class1, The Class2 class2, The Class3 Class3, and the Class4 Class!"
 	time.sleep(3)
 	def check_class():
 		global user_class
 		user_class = raw_input("Please choose your class: ")
-		class_list = ['Knight', 'Wizard', 'Monk', 'Thief']
+		class_list = ['Class1', 'Class2', 'Class3', 'Class4']
 		if user_class in class_list:
 			time.sleep(3)
 			print "Wait..... This cant be"
-			time.sleep(3)
+			time.sleep(3) 
 			print "You are " + user_name + " of the " + user_class + "s?"
 			time.sleep(3)
 			print "Is this really you???"
