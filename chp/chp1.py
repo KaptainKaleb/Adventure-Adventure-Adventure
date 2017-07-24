a09408fff9ed7ce670af617b33d3bd90f55823f9
James Vaughan <james.f.vaughan@outlook.com>
2017-07-24 16:09:09 -0300

    Added basic class (library) for user info, and laid out savegame structure

--- chp/chp1.py
+++ chp/chp1.py
@@ -1,5 +1,8 @@
+import sys
 from time import sleep
-from chp0 import weapon #Weapon? Gonna put that in chapter 0? # I guess I'll import it using my own method from the savegame.txt file
+from user import weapon #Weapon? Gonna put that in chapter 0? # I guess I'll import it using my own method from the savegame.txt file
+
+sys.path.insert(0, "/mnt/c/Users/James/Documents/projects/Adventure-Adventure-Adventure")
 
 def dialog00():
 	print "--In the Sewer--"
@@ -11,5 +14,5 @@ def dialog00():
 	print "*You venture a few steps in, and you immediately spot a giant rat*"
 	sleep(1)
 
-	print "*Preparing yourself for combat, you approach the rat with drawn*"
+	print "*Preparing yourself for combat, you approach the rat with " + weapon +" drawn*"
 	sleep(2)
