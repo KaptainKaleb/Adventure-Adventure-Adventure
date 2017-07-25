0fb07e2a140c79e553437aaae4699489cd3f6c11
James Vaughan <james.f.vaughan@outlook.com>
2017-07-24 23:15:13 -0300

    I hate how indentation matters in Python

--- user.py
+++ user.py
@@ -1,36 +1,38 @@
 from aaa import userInfo
 from lib.save import check
 
-if check() == true:
-    global name
-    name = userInfo[0]              # Character Name
-    global clss
-    clss = userInfo[1]              # Character Class
-    global wpn1
-    wpn1 = userInfo[2]              # Weapon 1 (Main)
-    global wpn2
-    wpn2 = userInfo[3]              # Weapon 2 (Offhand)
-    global inv
-    inv[0] = userInfo[4]            # Inventory 1
-    inv[1] = userInfo[5]            # Inventory 2
-    inv[2] = userInfo[6]            # Inventory 3
-    inv[3] = userInfo[7]            # Inventory 4
-    inv[4] = userInfo[8]            # Inventory 5
-    inv[5]= userInfo[9]             # Inventory 6
-    global lvl
-    lvl[0] = userInfo[10]           # Main Level
-    lvl[1] = userInfo[11]           # Fighting Level
-    lvl[2] = userInfo[12]           # Speech Level
-    lvl[3] = userInfo[13]           # Crafting Level
-    lvl[4] = userInfo[14]           # Agility Level
+def update():
+    if check() == true:
+        savegame = open("savegame.txt", "r")
+        global name
+        name = savegame.readline(0)              # Character Name
+        global clss
+        clss = savegame.readline(1)              # Character Class
+        global wpn1
+        wpn1 = savegame.readline(2)              # Weapon 1 (Main)
+        global wpn2
+        wpn2 = savegame.readline(3)              # Weapon 2 (Offhand)
+        global inv
+        inv[0] = savegame.readline(4)            # Inventory 1
+        inv[1] = savegame.readline(5)            # Inventory 2
+        inv[2] = savegame.readline(6)            # Inventory 3
+        inv[3] = savegame.readline(7)            # Inventory 4
+        inv[4] = savegame.readline(8)            # Inventory 5
+        inv[5]= savegame.readline(9)             # Inventory 6
+        global lvl
+        lvl[0] = savegame.readline(10)           # Main Level
+        lvl[1] = savegame.readline(11)           # Fighting Level
+        lvl[2] = savegame.readline(12)           # Speech Level
+        lvl[3] = savegame.readline(13)           # Crafting Level
+        lvl[4] = savegame.readline(14)           # Agility Level
 
-    if clss == 3:
-        global spl
-        spl[0] = userInfo[15]       # Spell 1
-        spl[1] = userInfo[16]       # Spell 2
-        spl[2] = userInfo[17]       # Spell 3
+        if clss == 3:
+            global spl
+            spl[0] = savegame.readline(15)       # Spell 1
+            spl[1] = savegame.readline(16)       # Spell 2
+            spl[2] = savegame.readline(17)       # Spell 3
 
-    global pgrs
-    pgrs = userInfo[18]             # Progress
-    global htpt
-    htpt = userInfo[19]             # Hitpoints (Health)
+        global pgrs
+        pgrs = savegame.readline(18)             # Progress
+        global htpt
+        htpt = savegame.readline(19)             # Hitpoints (Health)
